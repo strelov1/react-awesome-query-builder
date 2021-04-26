d551bc83ea5ace6249f9c02e34ec04a7a894f326
strelov1 <strelov1@gmail.com>
2021-04-26 10:03:19 +0300

    feat(sql-func): Add sql functions

--- packages/example/src/config.ts
+++ packages/example/src/config.ts
@@ -19,20 +19,221 @@ const config = {
   },
   funcs: {
     LOWER: {
-      label: 'Lowercase',
+      label: 'LOWER',
       returnType: 'text',
-      sqlFormatValue: (val, fieldDef, wgtDef, op, opDef) => {
-        console.log('SQL', val, fieldDef, wgtDef, op, opDef);
-        return SqlString.escape(val);
-      },
       args: {
-        first: {
+        str: {
           label: 'String',
           type: 'text',
           valueSources: ['value', 'field'],
         },
-        second: {
+      },
+    },
+    UPPER: {
+      label: 'UPPER',
+      returnType: 'text',
+      args: {
+        str: {
           label: 'String',
+          type: 'text',
+          valueSources: ['value', 'field'],
+        },
+      },
+    },
+    TRIM: {
+      label: 'TRIM',
+      returnType: 'text',
+      args: {
+        str: {
+          label: 'String',
+          type: 'text',
+          valueSources: ['value', 'field'],
+        },
+      },
+    },
+    LENGTH: {
+      label: 'LENGTH',
+      returnType: 'text',
+      args: {
+        str: {
+          label: 'String',
+          type: 'text',
+          valueSources: ['value', 'field'],
+        },
+      },
+    },
+    TO_DATE: {
+      label: 'TO_DATE',
+      returnType: 'text',
+      args: {
+        str: {
+          label: 'String',
+          type: 'text',
+          valueSources: ['value', 'field'],
+        },
+      },
+    },
+    LAST_DAY: {
+      label: 'LAST_DAY',
+      returnType: 'text',
+      args: {
+        str: {
+          label: 'String',
+          type: 'text',
+          valueSources: ['value', 'field'],
+        },
+      },
+    },
+    // two args
+    DATEDIFF: {
+      label: 'DATEDIFF',
+      returnType: 'text',
+      args: {
+        enddate: {
+          label: 'end date',
+          type: 'text',
+          valueSources: ['value', 'field'],
+        },
+        startdate: {
+          label: 'start date',
+          type: 'text',
+          valueSources: ['value', 'field'],
+        },
+      },
+    },
+    DATE_ADD: {
+      label: 'DATE_ADD',
+      returnType: 'text',
+      args: {
+        enddate: {
+          label: 'end date',
+          type: 'text',
+          valueSources: ['value', 'field'],
+        },
+        days: {
+          label: 'days',
+          type: 'text',
+          valueSources: ['value', 'field'],
+        },
+      },
+    },
+    DATE_SUB: {
+      label: 'DATE_SUB',
+      returnType: 'text',
+      args: {
+        enddate: {
+          label: 'end date',
+          type: 'text',
+          valueSources: ['value', 'field'],
+        },
+        days: {
+          label: 'days',
+          type: 'text',
+          valueSources: ['value', 'field'],
+        },
+      },
+    },
+    ADD_MOUNTHS: {
+      label: 'ADD_MOUNTHS',
+      returnType: 'text',
+      args: {
+        startdate: {
+          label: 'startdate',
+          type: 'text',
+          valueSources: ['value', 'field'],
+        },
+        days: {
+          label: 'days',
+          type: 'text',
+          valueSources: ['value', 'field'],
+        },
+      },
+    },
+    // three args
+    SUBSTR: {
+      label: 'SUBSTR',
+      returnType: 'text',
+      args: {
+        str: {
+          label: 'String',
+          type: 'text',
+          valueSources: ['value', 'field'],
+        },
+        start: {
+          label: 'start',
+          type: 'text',
+          valueSources: ['value'],
+        },
+        length: {
+          label: 'length',
+          type: 'text',
+          valueSources: ['value'],
+        },
+      },
+    },
+    REGEXP_EXTRACT: {
+      label: 'SUBSTR',
+      returnType: 'text',
+      args: {
+        str: {
+          label: 'String',
+          type: 'text',
+          valueSources: ['value', 'field'],
+        },
+        pattern: {
+          label: 'Pattern',
+          type: 'text',
+          valueSources: ['value'],
+        },
+        index: {
+          label: 'index',
+          type: 'text',
+          valueSources: ['value'],
+        },
+      },
+    },
+    REGEXP_REPLACE: {
+      label: 'SUBSTR',
+      returnType: 'text',
+      args: {
+        str: {
+          label: 'String',
+          type: 'text',
+          valueSources: ['value', 'field'],
+        },
+        pattern: {
+          label: 'Pattern',
+          type: 'text',
+          valueSources: ['value'],
+        },
+        replacement: {
+          label: 'replacement',
+          type: 'text',
+          valueSources: ['value'],
+        },
+      },
+    },
+    COALESCE: {
+      label: 'COALESCE',
+      returnType: 'text',
+      args: {
+        val1: {
+          label: 'val1',
+          type: 'text',
+          valueSources: ['value', 'field'],
+        },
+        val2: {
+          label: 'val2',
+          type: 'text',
+          valueSources: ['value', 'field'],
+        },
+        val3: {
+          label: 'val3',
+          type: 'text',
+          valueSources: ['value', 'field'],
+        },
+        val4: {
+          label: 'val4',
           type: 'text',
           valueSources: ['value', 'field'],
         },
@@ -41,10 +242,6 @@ const config = {
     EXPRESION: {
       label: 'EXPRESION',
       returnType: 'text',
-      sqlFormatValue: (val, fieldDef, wgtDef, op, opDef) => {
-        console.log('SQL', val, fieldDef, wgtDef, op, opDef);
-        return SqlString.escape(val);
-      },
       args: {
         first: {
           label: 'String',
